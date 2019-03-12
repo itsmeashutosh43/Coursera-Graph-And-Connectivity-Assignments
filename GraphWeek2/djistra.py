366c866cd017687bc3664efd5038c210fe7540a7
Ashutosh Chapagain <ashutosh.chapagain@student.ku.edu.np>
2019-03-12 21:06:22 +0545

    1 and 2 completed

---
+++ GraphWeek2/djistra.py
@@ -0,0 +1,83 @@
+
+from collections import defaultdict
+
+
+def findMin(dist):
+
+	minimum=min(dist.values())
+
+	for i,j in dist.items():
+		if j==minimum:
+			return i
+
+	return
+
+
+def djistraAlgo(adj,source):
+
+	Q=[]
+	Visited=[]
+	
+	for vertex in adj:
+		dist[vertex]=1000000
+		Q.append(vertex)
+
+	dist[source]=0
+	diffDist=dist.copy()
+
+
+	while Q:
+		u=findMin(dist)
+		Q.pop
+		Visited.append(u)
+		for neighbour in adj[u]:
+			if neighbour in Visited:
+				pass
+			else:
+				x=dist[u]+adj[u][neighbour]
+				if dist[neighbour] >x:
+					dist[neighbour] = x 
+					diffDist[neighbour] = x
+
+		Q.remove(u)
+		dist.pop(u)
+
+	return diffDist
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+content = open('djistra.txt', 'r').readlines()
+
+adj=defaultdict(dict)
+dist=defaultdict()
+
+for line in content:
+	preproccess=line.strip().split('\t')
+	index=int(preproccess[0])
+	for i in preproccess[1:]:
+		noComma=i.split(',')
+		adj[index][int(noComma[0])]=int(noComma[1])
+		
+		#adj[index].append((int(noComma[0]),int(noComma[1])))
+
+dist=djistraAlgo(adj,1)
+
+queries=[7,37,59,82,99,115,133,165,188,197]
+
+for a in answers:
+	print(dist[a])
+
+	
+
