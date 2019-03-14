412534fac01058e9862e5bc49202b376548db11c
Ashutosh Chapagain <ashutosh.chapagain@student.ku.edu.np>
2019-03-14 18:32:33 +0545

    Week 4 completed

--- GraphWeek4/2Sum.py
+++ GraphWeek4/2Sum.py
@@ -1,3 +1,18 @@
+'''
+The goal of this problem is to implement a variant of the 2-SUM algorithm covered in this week's lectures.
+
+The file contains 1 million integers, both positive and negative (there might be some repetitions!).This is your array of integers, with the ithi^{th}ith row of the file specifying the ithi^{th}ith entry of the array.
+
+Your task is to compute the number of target values ttt in the interval [-10000,10000] (inclusive) such that there are distinct numbers x,yx,yx,y in the input file that satisfy x+y=tx+y=tx+y=t. (NOTE: ensuring distinctness requires a one-line addition to the algorithm from lecture.)
+
+Write your numeric answer (an integer between 0 and 20001) in the space provided.
+
+
+'''
+
+
+
+
 content=open('2Sum.txt','r').readlines()
 from collections import defaultdict,Counter
 from bisect import bisect
